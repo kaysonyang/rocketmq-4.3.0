6ae614ef625a2f471193c22c0b3e9f5f4ee0f352
yangxinchu <yangxc@missfresh.cn>
2020-01-06 15:39:39 +0800

    源码调试

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -48,7 +48,8 @@ public class Consumer {
          * }
          * </pre>
          */
-
+        //设置nameserver addr
+        consumer.setNamesrvAddr("127.0.0.1:9876");
         /*
          * Specify where to start in case the specified consumer group is a brand new one.
          */
